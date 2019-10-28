73dc5e9d8becb6ee73146c89e3956bc3b9846bf6
tsapliy98 <54512764+tsapliy98@users.noreply.github.com>
2019-10-28 07:19:34 +0200

    Update MBR.sh

--- MBR.sh
+++ MBR.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
         o
         n
@@ -31,4 +30,4 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
         2
         82
         w
-    EOF
+EOF
